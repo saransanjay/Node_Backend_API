157667cdb6deec8e335e293ae959f1f5a6f2aa9e
Saran Sanjay <saransanjay96@gmail.com>
2026-01-18 23:19:37 +0530

    Mongo DB Connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import donenv from 'dotenv';
-import bootcamps from './routes/bootcamps.js';
-
+import connectDB from './config/db.js';
 
 // load evn vars
 donenv.config({ path: './config/config.env' });
+// Connect to database
+connectDB();
+// Route files
+import bootcamps from './routes/bootcamps.js';
+
 
 const app = express();
 
@@ -13,4 +17,11 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on the PORT ${PORT}`)); 
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on the PORT ${PORT}`));
+
+///  Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    ////  Close server & exist process
+    server.close(() => process.exit(1));
+})
