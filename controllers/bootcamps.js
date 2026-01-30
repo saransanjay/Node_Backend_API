263d0c5b86e044c017e2e13392d7329b61104aed
Saran Sanjay <saransanjay96@gmail.com>
2026-01-30 20:57:37 +0530

    Sort Query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,7 +17,7 @@ export const getBootcamps = asyncHandler(async (req, res, next) => {
     let reqQuery = { ...req.query };
 
     //Fields to exclude
-    const removeFields = ['select'];
+    const removeFields = ['select', 'sort'];
 
     // Loop over removeFields and delete then from reqQuery
     removeFields.forEach(param => delete reqQuery[param])
@@ -33,9 +33,21 @@ export const getBootcamps = asyncHandler(async (req, res, next) => {
 
     // Select Fields
     if (req.query.select) {
-        const fields = req.query.select.split(',').join(" ");
+        const fields = req.query.select.split(',').join(' ');
         query = query.select(fields);
     }
+
+    // Sort 
+    if (req.query.sort) {
+        const sortBy = req.query.sort.split(',').join(' ');
+        query = query.sort(sortBy);
+    }
+    else {
+
+        query = query.sort('-createdAt');
+
+    }
+
     // Executing query
     const bootcamp = await query;
 
