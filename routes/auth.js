e35412cf0c34a37319b2514ae02beec54fe2a013
Saran Sanjay <saransanjay96@gmail.com>
2026-02-11 23:34:46 +0530

    get me by token

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { register, login } from '../controllers/auth.js';
+import { register, login, getMe } from '../controllers/auth.js';
+import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/me').post(protect, getMe);
+
 
 
 export default router;
